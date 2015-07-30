6dbc7348bda2a8a4c309db4b325ca65a7dbe5028
liuzhecodekiller <qianfeng@qianfengdeMacBook-Pro-liuzhe.local>
2015-07-30 21:18:05 +0800

    Initial Commit

---
+++ scrollViewAddPicture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  scrollViewAddPicture
+//
+//  Created by qianfeng on 15-7-30.
+//  Copyright (c) 2015年 LZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ scrollViewAddPicture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  scrollViewAddPicture
+//
+//  Created by qianfeng on 15-7-30.
+//  Copyright (c) 2015年 LZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
